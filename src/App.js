e5bb41ca99d55e665573934bf12ab00902861479
Evan Procter <evanprocter@gmail.com>
2018-12-06 08:44:43 -0500

    changed Square component into a function component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={function() {
-        alert('click');
-      }}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={() => 
+        this.props.onClick()}
+      >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value= {this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+       />
+     );
   }
 
   render() {
